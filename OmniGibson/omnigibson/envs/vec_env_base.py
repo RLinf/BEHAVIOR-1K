7d150e552bc85c52e153b663a0bc9d39bc36f349
Jiaxing Qiu <jx.qiu@outlook.com>
2026-05-19 08:56:46 +0000

    feat: support scoped vector environment stepping (required for pipeline stage support in rlinf)
    Signed-off-by: Jiaxing Qiu <jx.qiu@outlook.com>

--- OmniGibson/omnigibson/envs/vec_env_base.py
+++ OmniGibson/omnigibson/envs/vec_env_base.py
@@ -23,34 +23,53 @@ class VectorEnvironment:
         for env in self.envs:
             env.post_play_load()
 
-    def step(self, actions, get_obs=True, render=True):
-        observations, rewards, terminates, truncates, infos = [], [], [], [], []
-        for i, action in enumerate(actions):
-            self.envs[i]._pre_step(action)
-        with og.sim.render_on_step(render):
-            og.sim.step()
-        for i, action in enumerate(actions):
-            obs, reward, terminated, truncated, info = self.envs[i]._post_step(
-                action, get_obs=get_obs
-            )
-            observations.append(obs)
-            rewards.append(reward)
-            terminates.append(terminated)
-            truncates.append(truncated)
-            infos.append(info)
+    def step(self, actions, env_indices=None, get_obs=True, render=True):
+        # When stepping a subset, we scope ``og.sim._scenes`` to the
+        # requested envs so the simulator only advances those scenes.
+        scoped = env_indices is not None
+        if scoped:
+            from omnigibson.utils.usd_utils import ControllableObjectViewAPI
+            view_dict_before = ControllableObjectViewAPI._VIEWS_BY_PATTERN
+            all_scenes = og.sim._scenes
+            og.sim._scenes = [all_scenes[idx] for idx in env_indices]
+            indices = env_indices
+        else:
+            indices = range(self.num_envs)
+
+        try:
+            for idx, action in zip(indices, actions):
+                self.envs[idx]._pre_step(action)
+            with og.sim.render_on_step(render):
+                og.sim.step()
+            observations, rewards, terminates, truncates, infos = [], [], [], [], []
+            for idx, action in zip(indices, actions):
+                obs, reward, terminated, truncated, info = self.envs[idx]._post_step(
+                    action, get_obs=get_obs
+                )
+                observations.append(obs)
+                rewards.append(reward)
+                terminates.append(terminated)
+                truncates.append(truncated)
+                infos.append(info)
+        finally:
+            if scoped:
+                og.sim._scenes = all_scenes
+                if ControllableObjectViewAPI._VIEWS_BY_PATTERN is not view_dict_before:
+                    og.sim.update_handles()
         return observations, rewards, terminates, truncates, infos
 
-    def reset(self, get_obs=True, **kwargs):
+    def reset(self, env_indices=None, get_obs=True, **kwargs):
+        indices = range(self.num_envs) if env_indices is None else env_indices
         if get_obs:
             observations, infos = [], []
-            for env in self.envs:
-                obs, info = env.reset(get_obs=get_obs, **kwargs)
+            for idx in indices:
+                obs, info = self.envs[idx].reset(get_obs=get_obs, **kwargs)
                 observations.append(obs)
                 infos.append(info)
             return observations, infos
         else:
-            for env in self.envs:
-                env.reset(get_obs=get_obs, **kwargs)
+            for idx in indices:
+                self.envs[idx].reset(get_obs=get_obs, **kwargs)
 
     def close(self):
         pass
